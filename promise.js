eb3a8d54a271cf23ee8dbfc2fc417665f965533d
Andi Ikbal <anikanstd7@gmail.com>
2022-09-20 00:22:48 +0800

    Menambahkan Promise yang dapat menerima parameter emosi (marah atau tidak marah)

--- promise.js
+++ promise.js
@@ -1,7 +1,27 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve, reject) => {
+    let i = 0;
+    promiseTheaterIXX().then((value) => {
+      value.forEach(element => {
+        if (element.hasil == emosi) {
+          i = i + 1;
+        }
+      });
+      resolve(promiseTheaterVGC().then((value) => {
+        value.forEach(element => {
+          if (element.hasil == emosi) {
+            i = i + 1;
+          }
+        });
+        return i;
+      }));
+    });
+
+  });
+};
 
 module.exports = {
   promiseOutput,
